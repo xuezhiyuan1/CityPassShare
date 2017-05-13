eea4ab2c63721c6dd658ab8dbec6855923fd2a3b
“lixianglei” <1728276353@qq.com>
2017-05-13 16:24:06 +0800

    添加了一个Button监听

--- app/src/main/java/com/example/think/citypass/MainActivity.java
+++ app/src/main/java/com/example/think/citypass/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.think.citypass;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        button = (Button) findViewById(R.id.androidBtn);
     }
 }
