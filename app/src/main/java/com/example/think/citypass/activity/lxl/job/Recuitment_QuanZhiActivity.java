14f09ea7a322602e2ede54c6a8c137e80f936135
“lixianglei” <1728276353@qq.com>
2017-05-17 14:59:37 +0800

    刚传的

---
+++ app/src/main/java/com/example/think/citypass/activity/lxl/job/Recuitment_QuanZhiActivity.java
@@ -0,0 +1,34 @@
+package com.example.think.citypass.activity.lxl.job;
+
+import com.example.think.citypass.common.base.BaseActivity;
+
+/**
+ * Created by d on 2017/5/17.
+ */
+
+public class Recuitment_QuanZhiActivity extends BaseActivity {
+    @Override
+    protected int layoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+}
