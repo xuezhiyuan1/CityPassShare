7a1b5b9c5d8b1370b51b293e8832d80a62b254a9
xuezhiyaun1 <1311257372@qq.com>
2017-05-22 09:09:40 +0800

    薛智源522

--- app/src/main/java/com/example/think/citypass/activity/house/Activity_House.java
+++ app/src/main/java/com/example/think/citypass/activity/house/Activity_House.java
@@ -6,7 +6,7 @@ import com.example.think.citypass.common.base.BaseActivity;
 /**
  * Created by d on 2017/5/20.
  */
-
+ 
 public class Activity_House extends BaseActivity {
     @Override
     protected int layoutId() {
