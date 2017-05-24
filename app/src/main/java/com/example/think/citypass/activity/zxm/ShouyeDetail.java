3da3d998e0a5c61cbac9599ec2e0725d888e7f2c
LemonEn <17862028028@qq.com>
2017-05-24 11:47:39 +0800

    zxm  0524

---
+++ app/src/main/java/com/example/think/citypass/activity/zxm/ShouyeDetail.java
@@ -0,0 +1,34 @@
+package com.example.think.citypass.activity.zxm;
+
+import com.example.think.citypass.common.base.BaseActivity;
+
+/**
+ * Created by 张萌 on 2017/5/24.
+ */
+
+public class ShouyeDetail  extends BaseActivity {
+    @Override
+    protected int layoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+}
